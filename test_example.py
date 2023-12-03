ab2d7d03c5eccabb83759e0d4eaf4b943c728ea9
Sárdi Barnabás <sardi.barnabas@united-consult.hu>
2023-12-03 20:01:41 +0100

    aider: Refactor `test_example.py` to execute the `generated_code` as a function and run each test in the `test` list.
    # Aider chat conversation:
    
    ASSISTANT: To test the `generated_code` with `test`, we need to:
    
    1. Convert the `generated_code` string into a function.
    2. Execute the assertions in `test`.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    test_example.py
    ```python
    <<<<<<< SEARCH
    generated_code = """
    def generated_function_name(args):
        # Function implementation
    """
    =======
    import sys
    
    def run_tests():
        exec(generated_code, globals())
        for t in test:
            exec(t, globals())
    
    if __name__ == "__main__":
        run_tests()
    >>>>>>> REPLACE
    ```
    
    This will execute the `generated_code` as a function in the global scope, then run each test in the `test` list. If any of the tests fail, an `AssertionError` will be raised.

--- test_example.py
+++ test_example.py
@@ -6,8 +6,13 @@
 test = ['assert min_cost([[1, 2, 3], [4, 8, 2], [1, 5, 3]], 2, 2) == 8', 'assert min_cost([[2, 3, 4], [5, 9, 3], [2, 6, 4]], 2, 2) == 12', 'assert min_cost([[3, 4, 5], [6, 10, 4], [3, 7, 5]], 2, 2) == 16']
 
 
-generated_code = """
-def generated_function_name(args):
-    # Function implementation
-"""
+import sys
+
+def run_tests():
+    exec(generated_code, globals())
+    for t in test:
+        exec(t, globals())
+
+if __name__ == "__main__":
+    run_tests()
 
